df279daff665263aa3f08011cad73292476915bc
EMSMihail <emsmihail@gmail.com>
2023-06-12 22:24:00 +0600

    First connection of html and go codes

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	//"database/sql"
+	"net/http"
+
 	_ "github.com/lib/pq"
 )
 
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "quiz.html")
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+func mainPageHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "quiz.html")
+}
+
 func main() {
-	fmt.Println("Hello, Michael!")
+	//fmt.Println("Hello, Michael!")
+	http.HandleFunc("/", mainPageHandler)
+	http.ListenAndServe(":5000", nil)
 }
