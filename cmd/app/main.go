f2b2f1fa3a146f01a0e8a80e8977d6ca680aaaac
EMSMihail <emsmihail@gmail.com>
2023-06-16 19:13:13 +0600

    Finalized structure of project. Now after post request data will be transfered to db

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -8,56 +8,22 @@ import (
 	"net/http"
 
 	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// func mainPageHandler(w http.ResponseWriter, r *http.Request) {
-// 	//http.ServeFile(w, r, "/web/templates/login.html")
-// 	tmpl, err := http.Dir("../../web/templates").Open("login.html")
-// 	if err != nil {
-// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	defer tmpl.Close()
+func registrationPageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		// Если метод запроса POST, вызовите обработчик регистрации
+		db, err := connectToDB()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer db.Close()
+		registerUserHandler(w, r, db)
+		return
+	}
 
-// 	// Serve the HTML template
-// 	fi, err := tmpl.Stat()
-// 	if err != nil {
-// 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	http.ServeContent(w, r, "login.html", fi.ModTime(), tmpl)
-// }
-
-// func cssHandler(w http.ResponseWriter, r *http.Request) {
-// 	filePath := "../../web/static/login.css"
-// 	defer func() {
-// 		if err := recover(); err != nil {
-// 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-// 			// You can also log the error or take any other appropriate action
-// 		}
-// 	}()
-
-// 	http.ServeFile(w, r, filePath)
-// }
-
-// func loginHandler(w http.ResponseWriter, r *http.Request) {
-// 	data := map[string]interface{}{
-// 		"Title": "Login Page",
-// 	}
-
-// 	tmpl, err := template.ParseFiles("../../web/templates/login.html")
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	if err := tmpl.Execute(w, data); err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-func registrationHandler(w http.ResponseWriter, r *http.Request) {
 	data := map[string]interface{}{
 		"Title": "Registration Page",
 	}
@@ -74,6 +40,57 @@ func registrationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func registerUserHandler(w http.ResponseWriter, r *http.Request, db *sql.DB) {
+	// Получите данные из формы регистрации, например:
+	nickname := r.FormValue("nickname")
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+
+	// Проверьте, что все необходимые данные были заполнены
+	if nickname == "" || email == "" || password == "" {
+		http.Error(w, "Не заполнены все обязательные поля", http.StatusBadRequest)
+		return
+	}
+
+	// Создайте объект User и заполните его данными
+	user := User{
+		Nickname:     nickname,
+		Email:        email,
+		PasswordHash: hashPassword(password), // Предполагается, что у вас есть функция для хеширования паролей
+	}
+
+	// Сохраните пользователя в базе данных
+	err := saveUserToDB(db, user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Перенаправьте пользователя на страницу успешной регистрации или другую нужную вам страницу
+	http.Redirect(w, r, "/success", http.StatusFound)
+}
+
+func saveUserToDB(db *sql.DB, user User) error {
+	// Создайте SQL-запрос для вставки данных пользователя в таблицу "users"
+	query := `INSERT INTO users (nickname, email, password_hash) VALUES ($1, $2, $3)`
+	_, err := db.Exec(query, user.Nickname, user.Email, user.PasswordHash)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func hashPassword(password string) string {
+	// Генерация соли и хэширование пароля
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return string(hashedPassword)
+}
+
 func connectToDB() (*sql.DB, error) {
 	connStr := "postgres://aizek:1234@localhost/quizzes?sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
@@ -81,6 +98,42 @@ func connectToDB() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func getUsersFromDB(db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT * FROM users")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var users []User
+
+	for rows.Next() {
+		var user User
+		err := rows.Scan(&user.ID, &user.Nickname, &user.Email, &user.PasswordHash)
+		if err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return users, nil
+}
+
+func successPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Registration Successful!")
+}
+
+type User struct {
+	ID           int
+	Nickname     string
+	Email        string
+	PasswordHash string
 }
 
 func main() {
@@ -97,74 +150,23 @@ func main() {
 	}
 	fmt.Println("Подключение к базе данных успешно!")
 
-	rows, err := db.Query("SELECT * FROM users")
+	users, err := getUsersFromDB(db)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		var id int
-		var nickname, email, password_hash string
-		err := rows.Scan(&id, &nickname, &email, &password_hash)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("ID:", id, "| Username:", nickname, "| E-Mail:", email, "| Password_hash:", password_hash)
-	}
-	if err = rows.Err(); err != nil {
-		log.Fatal(err)
+	fmt.Println("Пользователи:")
+
+	for _, user := range users {
+		fmt.Println("ID:", user.ID, "| Username:", user.Nickname, "| E-Mail:", user.Email, "| Password_hash:", user.PasswordHash)
 	}
 
-	fs := http.FileServer(http.Dir("../../web/static"))
+	fs := http.FileServer(http.Dir("../../web/static/css"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
 	//http.HandleFunc("/", loginHandler)
-	http.HandleFunc("/", registrationHandler)
+	http.HandleFunc("/", registrationPageHandler)
+	http.HandleFunc("/success", successPageHandler)
 
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
-
-// func main() {
-// 	//fmt.Println("Hello, Michael!")
-// 	http.HandleFunc("/", mainPageHandler)
-// 	http.HandleFunc("/static/css/login.css", cssHandler)
-
-// 	fs := http.FileServer(http.Dir("../../web/static"))
-// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-// 	//http.ListenAndServe(":5000", nil)
-// 	//http.HandleFunc("/", mainPageHandler)
-// 	// http.HandleFunc("../../web/static/login.css", cssHandler)
-// 	http.ListenAndServe(":5000", nil)
-// }
-
-// func main() {
-// 	http.HandleFunc("/", mainPageHandler)
-
-// 	fs := http.FileServer(http.Dir("../../web/static"))
-// 	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-// 	http.HandleFunc("/static/css/login.css", cssHandler)
-
-// 	http.ListenAndServe(":5000", nil)
-// }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	tmpl, err := template.ParseFiles("templates/index.html")
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-
-// 	err = tmpl.Execute(w, nil)
-// 	if err != nil {
-// 		http.Error(w, err.Error(), http.StatusInternalServerError)
-// 		return
-// 	}
-// }
-
-// func main() {
-// 	http.HandleFunc("/", handler)
-// 	http.ListenAndServe(":5000", nil)
-// }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module Quizzes
 
 go 1.20
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/lib/pq v1.10.9 // indirect
+	golang.org/x/crypto v0.10.0 // indirect
+)
